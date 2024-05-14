1d04c35f3f650db663781beaf111fa48e4bb138a
Gello <gelloclemente@gmail.com>
2024-05-14 16:40:58 +0800

    add update-profile-feature

--- demo_project/base/views.py
+++ demo_project/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # rooms = [
 #     {'id': 1, 'name': 'Lets learn python!'},
@@ -164,4 +164,16 @@ def deleteMessage(request, pk):
             room.participants.remove(request.user)
         return redirect('room', pk = room.id)
     return render(request, 'base/delete.html', {'obj': message})
- 
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+        else:
+            messages.error(request, 'An Error occurred while registration')
+    return render(request, 'base/update_user.html', {'form': form})
